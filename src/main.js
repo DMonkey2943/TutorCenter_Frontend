faf9d04124396f42b8954db734dc64ec6175beb5
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-01-18 19:28:04 +0700

    add admin/parents view

--- src/main.js
+++ src/main.js
@@ -1,29 +1,27 @@
-import './assets/main.css'
-import 'ant-design-vue/dist/reset.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
-import 'bootstrap/dist/css/bootstrap-utilities.min.css'
+import "./assets/main.css";
+import "ant-design-vue/dist/reset.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-utilities.min.css";
 
-import {
-    Drawer,
-    List,
-    Menu,
-    Button,
-} from 'ant-design-vue'
+import { Drawer, List, Menu, Button, Card, Table, Modal } from "ant-design-vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(Drawer)
-app.use(List)
-app.use(Menu)
-app.use(Button)
+app.use(Drawer);
+app.use(List);
+app.use(Menu);
+app.use(Button);
+app.use(Card);
+app.use(Table);
+app.use(Modal);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- src/views/admin/Parents.vue
+++ src/views/admin/Parents.vue
@@ -1,7 +1,129 @@
-<template>Trang quản lý phụ huynh</template>
+<template>
+  <!-- <h3>Trang quản lý phụ huynh</h3> -->
+  <!-- <button @click="getAllParents">Click to get all parents</button> -->
+  <a-card title="QUẢN LÝ PHỤ HUYNH" style="width: 100%">
+    <div class="row">
+      <div class="col-12">
+        <a-table :dataSource="parents" :columns="columns" :scroll="{ x: 576 }">
+          <template #bodyCell="{ column, record }">
+            <template v-if="column.key === 'status'">
+              <span v-if="record.blocked_at" class="text-danger">
+                Tạm khóa
+              </span>
+            </template>
+            <template v-if="column.key === 'action'">
+              <span>
+                <a-button
+                  @click="showModal"
+                  type="primary"
+                  class="me-0 me-sm-2 mb-2 mb-sm-0"
+                >
+                  Sửa
+                </a-button>
+                <a-modal
+                  v-model:open="open"
+                  width="50%"
+                  title="Basic Modal"
+                  @ok="handleOk"
+                >
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </a-modal>
+              </span>
+              <span>
+                <a-button @click="showDeleteConfirm" type="primary" danger>
+                  Xóa
+                </a-button>
+              </span>
+            </template>
+          </template>
+        </a-table>
+      </div>
+    </div>
+  </a-card>
+</template>
 
 <script setup>
+import { ref, onMounted } from "vue";
 import { useMenuAdmin } from "@/stores/use-menu-admin";
+import ParentService from "@/services/parent.service";
+import { Modal } from "ant-design-vue";
 
 useMenuAdmin().onSelectedKeys(["admin-parents"]);
+
+const parents = ref([]);
+
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Họ tên",
+    dataIndex: ["user", "name"], // Sử dụng mảng để truy cập nested object
+    key: "name",
+  },
+  {
+    title: "Email",
+    dataIndex: ["user", "email"],
+    key: "email",
+  },
+  {
+    title: "SĐT",
+    dataIndex: ["user", "phone"],
+    key: "phone",
+  },
+  {
+    title: "Tình trạng",
+    dataIndex: "blocked_at",
+    key: "status",
+  },
+  {
+    title: "Tùy chọn",
+    key: "action",
+    fixed: "right",
+  },
+];
+
+const getAllParents = async () => {
+  try {
+    const response = await ParentService.index();
+    // console.log(a.data);
+    parents.value = response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const open = ref(false);
+const showModal = () => {
+  open.value = true;
+};
+const handleOk = (e) => {
+  console.log(e);
+  open.value = false;
+};
+
+const showDeleteConfirm = () => {
+  Modal.confirm({
+    title: "Bạn có chắc sẽ xóa phụ huynh này?",
+    // icon: createVNode(ExclamationCircleOutlined),
+    // content: "Some descriptions",
+    okText: "Xóa",
+    okType: "danger",
+    cancelText: "Hủy",
+    onOk() {
+      console.log("OK");
+    },
+    onCancel() {
+      console.log("Cancel");
+    },
+  });
+};
+
+onMounted(() => {
+  getAllParents();
+});
 </script>
