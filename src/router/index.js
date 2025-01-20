8ce3bf2ca7baf6e4c9b22aee0929563bac7db7b7
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-01-20 19:55:32 +0700

    add not found view

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import publicRoutes from './publicRoutes';
-import parentRoutes from './parentRoutes';
-import tutorRoutes from './tutorRoutes';
-import adminRoutes from './adminRoutes';
+import { createRouter, createWebHistory } from "vue-router";
+import publicRoutes from "./publicRoutes";
+import parentRoutes from "./parentRoutes";
+import tutorRoutes from "./tutorRoutes";
+import adminRoutes from "./adminRoutes";
 
 const routes = [
   ...publicRoutes,
   ...parentRoutes,
   ...tutorRoutes,
   ...adminRoutes,
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
