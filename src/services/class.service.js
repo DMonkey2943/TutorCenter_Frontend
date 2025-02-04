daf3634953721f191f488ef98da00e620b76898f
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-02-04 18:47:50 +0700

    add class service

---
+++ src/services/class.service.js
@@ -0,0 +1,29 @@
+import createApiClient from "./api.service";
+
+class ClassService {
+  constructor(baseUrl = "/api/classes") {
+    this.api = createApiClient(baseUrl);
+  }
+
+  async index() {
+    return (await this.api.get("/")).data;
+  }
+
+  async store(request) {
+    return (await this.api.post("/", request)).data;
+  }
+
+  async show(id) {
+    return (await this.api.get(`/${id}`)).data;
+  }
+
+  async update(id, request) {
+    return (await this.api.patch(`/${id}`, request)).data;
+  }
+
+  async destroy(id) {
+    return (await this.api.delete(`/${id}`)).data;
+  }
+}
+
+export default new ClassService();
