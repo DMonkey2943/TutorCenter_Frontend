0623980eb6309c6551d0045343d16631d0b5cac6
DMonkey2943 <ltnduyen29042003@gmail.com>
2025-03-26 14:49:43 +0700

    style: implement sticky footer using flexbox

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="container-fluid bg-white p-0">
-    <!-- <h1>Main Layout</h1> -->
-    <!-- <header>Đây là header</header> -->
-     <TheNavbar></TheNavbar>
-    <router-view></router-view>
-     <TheFooter></TheFooter>
-
-    
+  <div class="page-container">
+    <div class="content-wrap">
+      <TheNavbar></TheNavbar>
+      <router-view></router-view>
+    </div>
+    <TheFooter class="footer"></TheFooter>
   </div>
 </template>
+
 <script setup>
-import TheNavbar from '@/components/TheNavbar.vue';
-import TheFooter from '@/components/TheFooter.vue';
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheFooter from "@/components/TheFooter.vue";
 </script>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; /* Chiếm toàn bộ chiều cao màn hình */
+}
+
+.content-wrap {
+  flex: 1 0 auto; /* Cho phép nội dung mở rộng */
+}
+
+.footer {
+  flex-shrink: 0; /* Ngăn footer co lại */
+}
+</style>
